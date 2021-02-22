7bffe869e5e5702c4fee631efa3eb7948d3a8b30
Hiram Guerrero <hiramg90@gmail.com>
2021-02-22 12:26:49 -0600

    1. Build a schema

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const {ApolloServer} = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({typeDefs});
+
+server.listen()
+  .then(() => {
+    console.log(`
+      Server is running!
+      Listening on port 4000
+      Explote at https://studio.apollographql.com/dev
+    `);
+  })
+  .catch();
